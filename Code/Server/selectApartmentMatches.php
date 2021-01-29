e3309a300681ba913b209912fc7688477bc3434e
Orrakel <43992953+Orrakel@users.noreply.github.com>
2021-01-29 09:31:51 +0100

    Add files via upload

---
+++ Code/Server/selectApartmentMatches.php
@@ -0,0 +1,40 @@
+<?php
+
+$db = new mysqli("localhost", "flatAdmin", "admin", "flatchmatch");
+
+if ($db->connect_errno) {
+    die("Verbindung fehlgeschlagen: " . $db->connect_error);
+}
+
+$sql = "select * from users ";
+$sql .= " where email in(SELECT Usersemail FROM matches";
+$sql .= " WHERE city = '" . $_GET["city"] . "'";
+$sql .= " AND zip = '" . $_GET["zip"] . "'";
+$sql .= " AND street = '" . $_GET["street"] . "'";
+$sql .= " AND housenumber = '" . $_GET["housenumber"] . "'";
+$sql .= ")";
+
+$output = "";
+
+$output .= '{"users":[';
+
+foreach ($db->query($sql) as $row) {
+   $output .= '{';
+   $output .= '"email":' . $row['email'] . ",";
+   $output .= '"firstname":' . $row['Firstname'] . ",";
+   $output .= '"lastname":' . $row['Lastname'] . ",";
+   $output .= '"age":' . $row['age'] . ",";
+   $output .= '"picture":' . $row['picture'] . ",";
+   $output .= '"income":' . $row['income'] . ",";  
+   $output .= '"job":' . $row['job'] . ",";  
+   $output .= '"schufa":' . $row['schufa'] . ",";  
+   $output .= '"pet":' . $row['pet'] . ",";  
+   $output .= '"persons":' . $row['persons'];  
+   $output .= '},';  
+}
+
+$output .= "]}";
+
+echo $output;
+
+?>
--- Code/Server/selectFilteredApartment.php
+++ Code/Server/selectFilteredApartment.php
@@ -36,8 +36,6 @@ if(isset($_GET["furnishing"]) && $_GET["furnishing"] != 0 && $_GET["petsAllowed"
 	$sql .= " and furnishing = '" . $_GET["furnishing"] . "'";
 }
 
-echo $sql;
-
 $output = "";
 
 $output .= '{"apartments":[';
